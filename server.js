dc4ca0883032ce147b6b68da61a6e1a92a283141
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2020-12-31 17:30:47 -0500

    basic swagger working

--- server.js
+++ server.js
@@ -14,28 +14,33 @@ app.use('/fruits', require('./routes/fruits'))
 const PORT = process.env.PORT || 3000
 
 const options = {
-definition: {
-    openapi: '3.0.0',
-    info: {
-        title: 'API Testing',
-        version: '0.1.0',
-        description: 'This is an app for swagger testing',
-        contact: {
-            name: 'Sara Hunter',
-            email: 'sarashunter@gmail.com'
-        }
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Testing',
+            version: '0.1.0',
+            description: 'This is an app for swagger testing',
+            license: {
+                name: 'MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'Sara Hunter',
+                email: 'sarashunter@gmail.com',
+                url: 'http://blahs.com',
+            }
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/fruits'
+            }
+        ]
     },
-    servers: [
-        {
-            url: 'http://localhost:3000/fruits'
-        }
-    ]
-},
-apis: ['./routes/fruits.js']
+    apis: ['./routes/fruits.js']
 }
 
 const specs = swaggerJsdoc(options)
-app.use('api-docs',
+app.use('/api-docs',
 swaggerUi.serve,
 swaggerUi.setup(specs))
 
