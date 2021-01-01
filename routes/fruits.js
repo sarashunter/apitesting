c42a41811d481176d840c3ab0ed7ac75b8957536
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2020-12-31 20:28:53 -0500

    Added first swagger path

--- routes/fruits.js
+++ routes/fruits.js
@@ -13,24 +13,36 @@
  *          name:
  *            type: string
  *            description: The name of the fruit.
- *          colors:
- *            type: array
- *            description: The colors of the fruit
  *          createdAt:
  *            type: string
  *            format: date
  *            description: The date of the record creation.
  *        example:
- *           name: Kiwi,
- *           colors: ['green']
- *
-*/        
+ *           name: Kiwi
+ * tags: 
+ *      name: Fruits
+ *      description: API for fruits for swagger testing
+ * paths:
+ *      /fruits/:
+ *          get:
+ *              summary: Returns all the fruit
+ *              tags: [Fruits]
+ *              responses:
+ *                  '200':
+ *                      description: the list of fruits
+ *                      content:
+ *                          application/json:
+ *                              schema:
+ *                                  type: integer
+*/
+
+        
 const express = require('express')
 const router = express.Router()
 
 const fruits = require('../util/data')
 
-router.get('/', (req, res) => {
+router.get('/fruits', (req, res) => {
     res.status(200).json(fruits)
 })
 
