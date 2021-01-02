31f857fa665d9c3f1a6db7e7b539d718ba3b7858
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2021-01-01 20:48:12 -0500

    Adding more routes

--- routes/fruits.js
+++ routes/fruits.js
@@ -22,7 +22,15 @@
  * tags:
  *      name: Fruits
  *      description: API for fruits for swagger testing
- * paths:
+ */
+
+const express = require('express')
+const router = express.Router()
+
+const fruits = require('../util/data')
+/**
+ * @swagger
+ *  paths:
  *      /fruits/:
  *          get:
  *              summary: Returns all the fruit
@@ -36,13 +44,78 @@
  *                                  type: integer
  */
 
-const express = require('express')
-const router = express.Router()
-
-const fruits = require('../util/data')
-
-router.get('/fruits', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json(fruits)
+})
+
+/**
+ * @swagger
+ *  paths:
+ *    /fruits/{id}:
+ *      get:
+ *        summary: Returns fruit with given id
+ *        tags: [Fruits]
+ *        parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: The fruit id
+ *        responses:
+ *          '200':
+ *            description: the list of fruits
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: integer
+ */
+router.get('/:id', (req, res) => {
+  const fruit = fruits.find(item => {
+    return item.id == req.params.id
+  })
+  fruit ? res.status(200).json(fruit) : res.sendStatus(404)
+})
+
+/**
+ * @swagger
+ *  paths:
+ *    /fruits/:
+ *      post:
+ *        summary: Creates a new fruit
+ *        tags: [Fruits]
+ *        requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  name:
+ *                    type: string
+ *        responses:
+ *          '200':
+ *            description: The created fruit
+ *            content:
+ *             application/json:
+ *               schema:
+ *                type: object
+ *                properties:
+ *                  name:
+ *                    type: string
+ *
+ */
+router.post('/', (req, res) => {
+  const { name } = req.body
+
+  const fruit = {
+    id: fruits.length + 1,
+    name,
+    createdAt: new Date()
+  }
+
+  fruits.push(fruit)
+  res.status(201).json(fruit)
 })
 
 module.exports = router
